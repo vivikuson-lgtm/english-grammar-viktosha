5f58558b0c5c6e6ed80199442baf4353434143f8
Victoria <victoria@Victorias-MacBook-Pro.local>
2025-12-29 22:19:49 +0200

    changed vite.config.ts to work with vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        "figma:foundry-client-api"
+      ]
+    }
+  },
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
